77983d99478359eb48a13e0fcc410cd4ed86251f
Tobias Cohen <me@tobiascohen.com>
2010-05-13 17:36:53 +1000

    allow for multiple users with same email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
-  
-  def self.find_by_login_or_email login
-    find_by_login(login) || find_by_email(login)
+  acts_as_authentic do |c|
+    c.validate_email_field = false
   end
+  
+  validates_length_of :email, :within => 6..100
+  validates_format_of :email, :with => Authlogic::Regex.email,
+    :message => I18n.t('error_messages.email_invalid', :default => 'should look like an email address.')
 end
