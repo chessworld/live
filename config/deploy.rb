416b6c0bb3917c527c921c402b1e72419e84955d
Tobias Cohen <me@tobiascohen.com>
2011-01-17 10:38:21 +1100

    Update deployment file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,4 @@ set :repository,  "git@github.com:chessworld/live.git"
 set :scm, :git
 set :deploy_to, "/var/www/#{application}"
 
-server 'root@184.106.191.30', :app, :web, :db, :primary => true
+server 'root@chesskids.com.au', :app, :web, :db, :primary => true
