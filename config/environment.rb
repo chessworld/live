9653c41ca348f7378ba2c94e570b4dc33d1a9db3
root <root@smtp.chesskids.com.au>
2011-05-26 04:10:07 +0000

    Override to force UTF-8

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file
 
+Encoding.default_internal = 'UTF-8'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
