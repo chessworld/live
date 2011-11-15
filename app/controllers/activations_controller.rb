dbf8e0bf91aade0092d943c0334b88a58be1cf2e
Tobias Cohen <me@tobiascohen.com>
2011-11-15 16:10:24 +1100

    Redirect to homepage after account activation.

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -9,7 +9,7 @@ class ActivationsController < ApplicationController
       flash[:notice] = "Your account has been activated!"
       UserSession.create(@user, false) # Log user in manually
       @user.deliver_welcome!
-      redirect_to account_url
+      redirect_to root_url
     else
       render :action => :new
     end
