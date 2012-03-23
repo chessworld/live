50a3873488ee1af88b8034a0bd9e8b3142dc8f6a
Tobias Cohen <me@tobiascohen.com>
2012-03-23 14:09:40 +1100

    Updated from address.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,25 @@
 class UserMailer < ActionMailer::Base
+  FROM = "Chess Kids Online <info@chesskids.com.au>"
+  
   def password_reset_instructions user
+    from UserMailer::FROM
     subject 'Password Reset Instructions'
-    from    'Chess Kids Live'
     recipients user.email
     sent_on Time.now
     body  :password_reset_url => reset_password_user_path(user, :token => user.perishable_token, :only_path => false)
   end
 
   def activation_instructions(user)
+    from UserMailer::FROM
     subject       "Activation Instructions"
-    from          "mailer@chesskids.com.au"
     recipients    user.email
     sent_on       Time.now
     body          :account_activation_url => activate_url(user.perishable_token)
   end
 
   def welcome(user)
+    from UserMailer::FROM
     subject       "Welcome to the site!"
-    from          "mailer@chesskids.com.au"
     recipients    user.email
     sent_on       Time.now
     body          :root_url => root_url
