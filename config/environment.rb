1d7a13c247b8a702d95cd8796cef413b431665ac
Tobias Cohen <me@tobiascohen.com>
2012-03-30 12:13:30 +1100

    Added automatic revision marker to flash application link, to help prevent caching when application update.

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,8 @@ Encoding.default_internal = 'UTF-8'
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+$revision = `git rev-parse HEAD`
+
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
   config.action_mailer.delivery_method = :smtp
